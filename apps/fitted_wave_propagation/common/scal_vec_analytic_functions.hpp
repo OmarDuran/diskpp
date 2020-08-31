bae6d1ba7d710a8eece1485d292e07275c64af30
Omar Durán <omaryesiduran@gmail.com>
2020-08-31 05:29:48 +0200

    Update scal_vec_analytic_functions.hpp

--- apps/fitted_wave_propagation/common/scal_vec_analytic_functions.hpp
+++ apps/fitted_wave_propagation/common/scal_vec_analytic_functions.hpp
@@ -45,6 +45,19 @@ class scal_vec_analytic_functions
                         };
                 }
                 break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> static_vector<double, 2> {
+                            double x,y,ux,uy;
+                            x = pt.x();
+                            y = pt.y();
+                            ux = x*t*t*std::sin(M_PI*x)*std::sin(M_PI*y);
+                            uy = x*t*t*std::sin(M_PI*x)*std::sin(M_PI*y);
+                            static_vector<double, 2> u{ux,uy};
+                            return u;
+                        };
+                }
+                break;
             default:
             {
                 std::cout << " Function not implemented " << std::endl;
@@ -69,6 +82,19 @@ class scal_vec_analytic_functions
                             y = pt.y();
                             vx = -(std::sqrt(2)*M_PI*x*x*std::cos((M_PI*x)/2.)*std::sin(std::sqrt(2)*M_PI*t)*std::sin(M_PI*y));
                             vy = -(std::sqrt(2)*M_PI*x*x*std::cos((M_PI*x)/2.)*std::sin(std::sqrt(2)*M_PI*t)*std::sin(M_PI*y));
+                            static_vector<double, 2> v{vx,vy};
+                            return v;
+                        };
+                }
+                break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> static_vector<double, 2> {
+                            double x,y,vx,vy;
+                            x = pt.x();
+                            y = pt.y();
+                            vx = 2.0*t*x*std::sin(M_PI*x)*std::sin(M_PI*y);
+                            vy = 2.0*t*x*std::sin(M_PI*x)*std::sin(M_PI*y);
                             static_vector<double, 2> v{vx,vy};
                             return v;
                         };
@@ -106,6 +132,19 @@ class scal_vec_analytic_functions
                         };
                 }
                 break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> static_vector<double, 2> {
+                            double x,y,ax,ay;
+                            x = pt.x();
+                            y = pt.y();
+                            ax = 2.0*x*std::sin(M_PI*x)*std::sin(M_PI*y);
+                            ay = 2.0*x*std::sin(M_PI*x)*std::sin(M_PI*y);
+                            static_vector<double, 2> a{ax,ay};
+                            return a;
+                        };
+                }
+                break;
             default:
             {
                 std::cout << " Function not implemented " << std::endl;
@@ -130,6 +169,20 @@ class scal_vec_analytic_functions
                             y = pt.y();
                             fx = -(std::cos(std::sqrt(2)*M_PI*t)*(-4*M_PI*x*std::sin((M_PI*x)/2.)*(M_PI*x*std::cos(M_PI*y) + 6*std::sin(M_PI*y)) + std::cos((M_PI*x)/2.)*(16*M_PI*x*std::cos(M_PI*y) + (24 + M_PI*M_PI*x*x)*std::sin(M_PI*y))))/4.0;
                             fy = (std::cos(std::sqrt(2)*M_PI*t)*(4*M_PI*x*std::sin((M_PI*x)/2.)*(M_PI*x*std::cos(M_PI*y) + 2*std::sin(M_PI*y)) + std::cos((M_PI*x)/2.)*(-16*M_PI*x*std::cos(M_PI*y) + (-8 + 5*M_PI*M_PI*x*x)*std::sin(M_PI*y))))/4.;
+                            static_vector<double, 2> f{fx,fy};
+                            return f;
+                        };
+                }
+                break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> static_vector<double, 2> {
+                            double x,y,fx,fy;
+                            x = pt.x();
+                            y = pt.y();
+                            fx = -2*(M_PI*t*t*std::cos(M_PI*x)*(M_PI*x*std::cos(M_PI*y) + 3*std::sin(M_PI*y)) +
+                                 std::sin(M_PI*x)*(M_PI*t*t*std::cos(M_PI*y) - (1 + 2*M_PI*M_PI*t*t)*x*std::sin(M_PI*y)));
+                            fy = (1 + M_PI*M_PI*t*t)*x*std::cos(M_PI*(x - y)) - (1 + 3*M_PI*M_PI*t*t)*x*std::cos(M_PI*(x + y)) - 2*M_PI*t*t*std::sin(M_PI*(x + y));
                             static_vector<double, 2> f{fx,fy};
                             return f;
                         };
@@ -173,6 +226,24 @@ class scal_vec_analytic_functions
                         };
                 }
                 break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> static_matrix<double,2,2> {
+                            double x,y;
+                            x = pt.x();
+                            y = pt.y();
+                            static_matrix<double,2,2> sigma = static_matrix<double,2,2>::Zero(2,2);
+                            sigma(0,0) = t*t*(M_PI*x*std::cos(M_PI*y)*std::sin(M_PI*x) + 3*(M_PI*x*std::cos(M_PI*x) + std::sin(M_PI*x))*std::sin(M_PI*y));
+                        
+                            sigma(0,1) = t*t*(M_PI*x*std::cos(M_PI*y)*std::sin(M_PI*x) + (M_PI*x*std::cos(M_PI*x) + std::sin(M_PI*x))*std::sin(M_PI*y));
+                        
+                            sigma(1,0) = sigma(0,1);
+                        
+                            sigma(1,1) = t*t*(3*M_PI*x*std::cos(M_PI*y)*std::sin(M_PI*x) + (M_PI*x*std::cos(M_PI*x) + std::sin(M_PI*x))*std::sin(M_PI*y));
+                            return sigma;
+                        };
+                }
+                break;
             default:
             {
                 std::cout << " Function not implemented " << std::endl;
@@ -196,6 +267,16 @@ class scal_vec_analytic_functions
                             x = pt.x();
                             y = pt.y();
                             return x*x*std::sin(std::sqrt(2)*M_PI*t)*std::sin(M_PI*x)*std::sin(M_PI*y);
+                        };
+                }
+                break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> double {
+                            double x,y;
+                            x = pt.x();
+                            y = pt.y();
+                            return t*t*x*std::sin(M_PI*x)*std::sin(M_PI*y);
                         };
                 }
                 break;
@@ -224,6 +305,16 @@ class scal_vec_analytic_functions
                         };
                 }
                 break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> double {
+                            double x,y;
+                            x = pt.x();
+                            y = pt.y();
+                            return 2*t*x*std::sin(M_PI*x)*std::sin(M_PI*y);
+                        };
+                }
+                break;
             default:
             {
                 std::cout << " Function not implemented " << std::endl;
@@ -249,6 +340,16 @@ class scal_vec_analytic_functions
                         };
                 }
                 break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> double {
+                            double x,y;
+                            x = pt.x();
+                            y = pt.y();
+                            return 2*x*std::sin(M_PI*x)*std::sin(M_PI*y);
+                        };
+                }
+                break;
             default:
             {
                 std::cout << " Function not implemented " << std::endl;
@@ -271,6 +372,17 @@ class scal_vec_analytic_functions
                             x = pt.x();
                             y = pt.y();
                             f = -2*std::sin(std::sqrt(2)*M_PI*t)*(2*M_PI*x*std::cos(M_PI*x) + std::sin(M_PI*x))*std::sin(M_PI*y);
+                            return f;
+                        };
+                }
+                break;
+            case EFunctionQuadraticInTime:
+                {
+                    return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> double {
+                            double x,y,f;
+                            x = pt.x();
+                            y = pt.y();
+                            f = 2*(-(M_PI*t*t*std::cos(M_PI*x)) + (1 + M_PI*M_PI*t*t)*x*std::sin(M_PI*x))*std::sin(M_PI*y);
                             return f;
                         };
                 }
@@ -304,6 +416,20 @@ class scal_vec_analytic_functions
                         };
                 }
                 break;
+            case EFunctionQuadraticInTime:
+            {
+                return [&t](const typename disk::generic_mesh<double, 2>::point_type& pt) -> std::vector<double> {
+                        double x,y;
+                        x = pt.x();
+                        y = pt.y();
+                        std::vector<double> flux(2);
+                        flux[0] = M_PI*t*t*x*std::cos(M_PI*x)*std::sin(M_PI*y) + t*t*std::sin(M_PI*x)*std::sin(M_PI*y);
+                        flux[1] = M_PI*t*t*x*std::cos(M_PI*y)*std::sin(M_PI*x);
+                    
+                        return flux;
+                    };
+            }
+            break;
             default:
             {
                 std::cout << " Function not implemented " << std::endl;
